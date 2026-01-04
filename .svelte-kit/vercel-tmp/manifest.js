39d01f024a027b54bf20929ac50656de02ece4bf
suanna0 <117539182+suanna0@users.noreply.github.com>
2026-01-04 15:36:59 -0800

    upda

--- .svelte-kit/vercel-tmp/manifest.js
+++ .svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([]),
 	mimeTypes: {},
 	_: {
-		client: {start:"_app/immutable/entry/start.DXNwps4k.js",app:"_app/immutable/entry/app.DL8JbIoM.js",imports:["_app/immutable/entry/start.DXNwps4k.js","_app/immutable/chunks/D9L3EhYe.js","_app/immutable/chunks/CyF6-Xog.js","_app/immutable/chunks/D46IKvaC.js","_app/immutable/chunks/C0WzV_g1.js","_app/immutable/entry/app.DL8JbIoM.js","_app/immutable/chunks/CyF6-Xog.js","_app/immutable/chunks/CkAdUwA7.js","_app/immutable/chunks/n02RSbSe.js","_app/immutable/chunks/C0WzV_g1.js","_app/immutable/chunks/Bm3snW4G.js","_app/immutable/chunks/F6b9OAFo.js","_app/immutable/chunks/DoKfKMj9.js","_app/immutable/chunks/D46IKvaC.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.CAJtHh2O.js",app:"_app/immutable/entry/app.Bs4pr_jj.js",imports:["_app/immutable/entry/start.CAJtHh2O.js","_app/immutable/chunks/D_6ZYLMD.js","_app/immutable/chunks/CyF6-Xog.js","_app/immutable/chunks/D46IKvaC.js","_app/immutable/chunks/C0WzV_g1.js","_app/immutable/entry/app.Bs4pr_jj.js","_app/immutable/chunks/CyF6-Xog.js","_app/immutable/chunks/CkAdUwA7.js","_app/immutable/chunks/n02RSbSe.js","_app/immutable/chunks/C0WzV_g1.js","_app/immutable/chunks/Bm3snW4G.js","_app/immutable/chunks/F6b9OAFo.js","_app/immutable/chunks/DoKfKMj9.js","_app/immutable/chunks/D46IKvaC.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
